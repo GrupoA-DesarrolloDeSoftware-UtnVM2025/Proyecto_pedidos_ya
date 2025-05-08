ee1e00b3c6028341255eacf154e6aa14392275b0
Agustin Liendo <agusliendo@gmail.com>
2025-05-08 15:38:44 -0300

    Creadas todas las entidades que generarán la base de datos

--- src/delivery/delivery/delivery.ts
+++ src/delivery/delivery/delivery.ts
@@ -1,1 +1,30 @@
-export class Delivery {}
+import { Zona } from "src/zonas/zona/zona";
+import { PrimaryGeneratedColumn, Column, ManyToMany, Check } from "typeorm";
+
+@Check('"radius" > 0')
+@Check('"location" IS NOT NULL')
+@Check('"perosonId" IS NOT NULL')
+@Check('"status" IS NOT NULL')
+
+export class Delivery {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    perosonId: number;
+
+    @Column()
+    location: {latitude: number; longitude: number};
+
+    @Column('float')
+    radius: number;
+
+    @Column()
+    status: string;
+
+    @ManyToMany(() => Zona, (zona) => zona.deliveries)
+    zonas: Zona[];
+}
+
+
+
--- src/zonas/zona/zona.ts
+++ src/zonas/zona/zona.ts
@@ -1,1 +1,24 @@
-export class Zona {}
+import { Delivery } from 'src/delivery/delivery/delivery';
+import { Entity, PrimaryGeneratedColumn, Column, Check, ManyToMany } from 'typeorm';
+
+@Entity()
+@Check('"radius" > 0')
+@Check('"location" IS NOT NULL')
+@Check('"name" IS NOT NULL')
+
+export class Zona {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    location: {latitude: number; longitude: number};
+
+    @Column('float')
+    radius: number;
+
+    @ManyToMany(() => Delivery, (delivery) => delivery.zonas)
+    deliveries: Delivery[];
+}
