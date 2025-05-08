3a777e4f9ec359e1956819d2d49ba3c5a2761b38
Agustin Liendo <agusliendo@gmail.com>
2025-05-08 15:21:13 -0300

    a

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { HelloEntity } from './entities/hello.entity';
+
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  async getHellos(): Promise<HelloEntity[]> {
-    return await this.appService.find();
-  }
+
+
 }
