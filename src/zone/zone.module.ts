d3babeabf133bce1c3a50a2eb4c33fa119ab6bb2
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-05-26 22:50:31 -0300

    Incorporadas las funcionalidades de zone requeridas segun la documentación

--- src/zone/zone.module.ts
+++ src/zone/zone.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ZoneController } from './zone.controller';
 import { ZoneService } from './zone.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {ZoneEntity} from "../entities/zone.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ZoneEntity])],
   providers: [ZoneService],
-  controllers: [ZoneController]
+  controllers: [ZoneController],
+  exports: [TypeOrmModule]
 })
 export class ZoneModule {}
--- src/zone/zone.service.ts
+++ src/zone/zone.service.ts
@@ -1,29 +1,31 @@
 import {Injectable, NotFoundException} from '@nestjs/common';
 import {Repository} from "typeorm";
-import {Zone} from "../entities/zone";
+import {ZoneEntity} from "../entities/zone.entity";
 import {InjectRepository} from "@nestjs/typeorm";
 import {CreateZoneDto} from "../interfaces/create/create-zone-dto";
+import {UpdateZoneDto} from "../interfaces/update/update-zone.dto";
 import {PaginationDto} from "../interfaces/pagination.dto";
+import {PatchZoneDto} from "../interfaces/patch/patch-zone.dto";
 
 @Injectable()
 export class ZoneService {
-    private zoneRepository: Repository<Zone>;
+    private zoneRepository: Repository<ZoneEntity>;
 
     constructor(
-        @InjectRepository(Zone)
-        zoneRepository: Repository<Zone>,
+        @InjectRepository(ZoneEntity)
+        zoneRepository: Repository<ZoneEntity>,
     ) {
         this.zoneRepository = zoneRepository;
     }
 
     //Guardar una nueva zona
-    async create (createZoneDto: CreateZoneDto): Promise<Zone> {
+    async create (createZoneDto: CreateZoneDto): Promise<ZoneEntity> {
         const zone = this.zoneRepository.create(createZoneDto);
         return this.zoneRepository.save(zone);
     }
 
     //Buscar todas las zonas empleando paginacion
-    async findAll(paginationDto: PaginationDto): Promise<{zones: Zone[]; total: number}> {
+    async findAll(paginationDto: PaginationDto): Promise<{zones: ZoneEntity[]; total: number}> {
         const {limit, offset} = paginationDto;
 
         const [zones, total] = await this.zoneRepository.findAndCount({
@@ -33,12 +35,30 @@ export class ZoneService {
         return {zones, total};
     }
 
-    async findById(id: number): Promise<Zone> {
+    async findById(id: number): Promise<ZoneEntity> {
         const zone = await this.zoneRepository.findOne({where: {id} });
         if (!zone) {
             throw new NotFoundException(`Zona con id: ${id} no encontrada`);
         }
         return zone;
+    }
+
+    async update(id: number, updateZoneDto: UpdateZoneDto): Promise<ZoneEntity> {
+        const zone = await this.findById(id);
+        this.zoneRepository.merge(zone, updateZoneDto);
+        return this.zoneRepository.save(zone);
+    }
+
+    async patch(id: number, patchZoneDto: PatchZoneDto): Promise<ZoneEntity> {
+        const zone = await this.findById(id);
+        this.zoneRepository.merge(zone, patchZoneDto);
+        return this.zoneRepository.save(zone);
+    }
+
+    async remove(id: number): Promise<{ message: string }> {
+        const zone = await this.findById(id);
+        await this.zoneRepository.remove(zone);
+        return {message: 'Zona eliminada'};
     }
 
 
