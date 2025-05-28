ba02d25c09d9889a7074f408443f42b797dccb32
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-05-28 12:21:06 -0300

    Actualizado para restringir los valores máximos y mínmos que pueden tomar los valores de latitud y longitud

--- src/interfaces/location.dto.ts
+++ src/interfaces/location.dto.ts
@@ -1,11 +1,15 @@
-import {IsNotEmpty, IsNumber} from "class-validator";
+import {IsNotEmpty, IsNumber, Max, Min} from "class-validator";
 
 export class LocationDto {
     @IsNumber()
+    @Min(-90)
+    @Max(90)
     @IsNotEmpty({message: 'La latitud es obligatoria'})
     latitude: number;
 
     @IsNumber()
+    @Min(-180)
+    @Max(180)
     @IsNotEmpty({message: 'La longitud es obligatoria'})
     longitude: number;
 }
