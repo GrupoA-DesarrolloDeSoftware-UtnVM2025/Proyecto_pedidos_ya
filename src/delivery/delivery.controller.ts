4f7dc30b3bde759ee8b48db0f0668b9148ca9c4b
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-06-11 22:26:33 -0300

    Añadido AuthGuard y permisos específicos en DeliveryController para mejorar la gestión de acceso a las rutas

--- src/delivery/delivery.controller.ts
+++ src/delivery/delivery.controller.ts
@@ -16,50 +16,64 @@ import {AuthGuard} from "../middlewares/auth.middleware";
 export class DeliveryController {
     constructor(private readonly deliveryService: DeliveryService) {}
 
+    @UseGuards(AuthGuard)
+    @Permissions(['delivery_create'])
     @Post()
     create(@Body() createDeliveryDto: CreateDeliveryDto): Promise<DeliveryEntity> {
         return this.deliveryService.create(createDeliveryDto);
     }
 
     @UseGuards(AuthGuard)
-    @Permissions(['test2'])
+    @Permissions(['delivery_read'])
     @Get()
     findAll(@Query() paginationDto: PaginationDto): Promise<{deliveries: DeliveryEntity[]; total: number}> {
         return this.deliveryService.findAll(paginationDto);
     }
 
-
-
+    @UseGuards(AuthGuard)
+    @Permissions(['delivery_edit'])
     @Put(":id/location")
     updateLocation(@Param("id") id: number, @Body() updateLocationDto: UpdateDeliveryLocationDto): Promise<DeliveryEntity> {
         return this.deliveryService.updateLocation(id,updateLocationDto);
     }
 
+    @UseGuards(AuthGuard)
+    @Permissions(['delivery_edit'])
     @Put("id/status")
     updateStatus(@Param("id") id: number, @Body() updateStatusDto: UpdateDeliveryStatusDto): Promise<DeliveryEntity> {
         return this.deliveryService.updateStatus(id, updateStatusDto);
     }
 
+    @UseGuards(AuthGuard)
+    @Permissions(['delivery_delete'])
     @Delete(':id')
     remove(@Param('id') id: number): Promise <{message: string}> {
         return this.deliveryService.remove(id);
     }
 
+    @UseGuards(AuthGuard)
+    @Permissions(['delivery_zone_assignment'])
     @Post(":id/assignZone")
     assignZone(@Param('id') id: number, @Body() assignZoneDto: AssignZoneDto): Promise<DeliveryEntity> {
         return this.deliveryService.assignZone(id, assignZoneDto);
     }
 
+    @UseGuards(AuthGuard)
+    @Permissions(['delivery_read'])
     @Get(':id/zones')
     getZones(@Param('id') id: number): Promise<any[]> {
         return this.deliveryService.getZones(id);
     }
 
+    @UseGuards(AuthGuard)
+    @Permissions(['delivery_zone_assignment'])
     @Delete(":id/zone/:zoneId")
     removeZone(@Param('id') id: number, @Param('zoneId') zoneId: number): Promise<{message: string}> {
         return this.deliveryService.removeZone(id,zoneId);
     }
 
+    @UseGuards(AuthGuard)
+    @Permissions(['delivery_read','delivery_zone_assignment'])
     @Get('findByProximity')
     findByProximity(
         @Query('latitude') latitude: number,
@@ -72,12 +86,16 @@ export class DeliveryController {
 
     }
 
+    @UseGuards(AuthGuard)
+    @Permissions(['delivery_read'])
     @Get('findByZone')
     findByZone(@Query('id') id: number): Promise<DeliveryEntity[]> {
         const findByZoneDto: FindByZoneDto = { zoneId: id }
         return this.deliveryService.findByZone(findByZoneDto);
     }
 
+    @UseGuards(AuthGuard)
+    @Permissions(['delivery_read'])
     @Get(':id')
     findOne(@Param('id') id: number): Promise<DeliveryEntity> {
         return this.deliveryService.findOne(id);
